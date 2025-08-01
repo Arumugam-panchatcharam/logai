eead5dbe0529820375aa039cfa03297d41319681
Arumugam Panchatcharam <p.arumugam@telekom-digital.com>
2025-08-01 16:01:21 +0530

    added Telemetry parser

--- gui/callbacks/clustering.py
+++ gui/callbacks/clustering.py
@@ -57,6 +57,14 @@ def create_attribute_component(attributes):
         },
         style_header_conditional=[{"textAlign": "left"}],
         style_cell_conditional=[{"textAlign": "left"}],
+        style_header={
+            'backgroundColor': 'rgb(50, 50, 50)',
+            'color': 'white'
+        },
+        style_data={
+            'backgroundColor': 'white',
+            'color': 'black'
+        },
     )
 
     return html.Div(children=[table, html.Div(id="table-dropdown-container")])
@@ -180,6 +188,14 @@ def update_logline_list(data):
             page_action="native",
             page_size=20,
             page_current=0,
+            style_header={
+                'backgroundColor': 'rgb(50, 50, 50)',
+                'color': 'white'
+            },
+            style_data={
+                'backgroundColor': 'white',
+                'color': 'black'
+            },
         )
     else:
         return dash_table.DataTable()
